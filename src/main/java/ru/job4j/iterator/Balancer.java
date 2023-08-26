12d6b5b58bc5ed2be5f419bf6777d051cea5f070
dmn800 <nefediev.avid@yandex.ru>
2023-08-26 12:25:00 +0500

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes,
+                             Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (ArrayList<Integer> array : nodes) {
+                if (source.hasNext()) {
+                    array.add(source.next());
+                }
+            }
+        }
+    }
+}
