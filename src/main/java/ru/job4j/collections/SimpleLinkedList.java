904365b756e8eea2ff00e9e412589946b7f8d58d
dmn800 <nefediev.avid@yandex.ru>
2023-09-02 09:28:50 +0500

    2. Создать контейнер на базе связанного списка  [#159]

---
+++ src/main/java/ru/job4j/collections/SimpleLinkedList.java
@@ -0,0 +1,92 @@
+package ru.job4j.collections;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        Node<E> currNode = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            while (currNode.next != null) {
+                currNode = currNode.next;
+            }
+            currNode.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> node = head;
+        int count = 0;
+        if (index == 0) {
+            return head.item;
+        } else {
+            while (count < index) {
+                node = node.next;
+                count++;
+            }
+        }
+        return node.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int expectedModCount = modCount;
+            Node<E> nextIt = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return nextIt != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> curIt = nextIt;
+                nextIt = curIt.next;
+                return curIt.item;
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
