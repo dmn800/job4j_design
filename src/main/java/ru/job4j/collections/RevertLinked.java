8095beb735b6f86c54452e5445929edb14d92f11
dmn800 <nefediev.avid@yandex.ru>
2023-09-03 23:56:55 +0500

    6. Перевернуть связанный список [#161]

---
+++ src/main/java/ru/job4j/collections/RevertLinked.java
@@ -0,0 +1,72 @@
+package ru.job4j.collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RevertLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public boolean revert() {
+        boolean rsl;
+        if (head == null || head.next == null) {
+            rsl = false;
+        } else {
+            Node<T> prev = null;
+            Node<T> curr = head;
+            while (curr != null) {
+                Node<T> after = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = after;
+            }
+            head = prev;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+}
