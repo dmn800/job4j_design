004dcbc0456dd2e2444189a840f8f3a127ea4962
dmn800 <nefediev.avid@yandex.ru>
2023-11-17 01:10:19 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        boolean student = true;
+        char group = 'a';
+        byte code = 7;
+        double height = 1.9;
+        float id = 3234f;
+        long max = 525235423L;
+
+        LOG.debug("\nUser info name - {}, age - {}, student - {}, group - {}, "
+                        + "code - {}, height - {}, id - {}, max - {}",
+                name, age, student, group, code, height, id, max);
     }
 }
