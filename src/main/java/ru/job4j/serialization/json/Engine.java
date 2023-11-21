bc38a552c061fa4f58555b9f990663b35c2e5f00
dmn800 <nefediev.avid@yandex.ru>
2023-11-21 22:06:03 +0500

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Engine.java
+++ src/main/java/ru/job4j/serialization/json/Engine.java
@@ -1,9 +1,11 @@
 package ru.job4j.serialization.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Engine {
     private final boolean electric;
@@ -20,6 +22,26 @@ public class Engine {
         this.features = features;
     }
 
+    public boolean isElectric() {
+        return electric;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public TypeEngine getType() {
+        return type;
+    }
+
+    public String[] getFeatures() {
+        return features;
+    }
+
     @Override
     public String toString() {
         return "Engine{"
@@ -32,25 +54,24 @@ public class Engine {
     }
 
     public static void main(String[] args) {
+        JSONObject jsonTypeEngine = new JSONObject("{\"engine\":\"Fuel\"}");
+
+        List<String> list = new ArrayList<>();
+        list.add("1.8 L");
+        list.add("500 km");
+        JSONArray jsonFeatures = new JSONArray(list);
+
         final Engine engine = new Engine(true, 1000000,
                 "Tesla", new TypeEngine("Electric"), new String[] {"25 MW", "400 km"});
 
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(engine));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("electric", engine.isElectric());
+        jsonObject.put("price", engine.getPrice());
+        jsonObject.put("company", engine.getCompany());
+        jsonObject.put("type", jsonTypeEngine);
+        jsonObject.put("features", jsonFeatures);
 
-        final String engineJson =
-                "{"
-                        + "\"electric\":false,"
-                        + "\"price\":500000.0,"
-                        + "\"company\":\"Rolls-Royce\","
-                        + "\"type\":"
-                        + "{"
-                        + "\"type\":\"Fuel\""
-                        + "},"
-                        + "\"features\":"
-                        + "[\"1.8 L\", \"500 km\"]"
-                        + "}";
-        final Engine engineMod = gson.fromJson(engineJson, Engine.class);
-        System.out.println(engineMod);
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(engine));
     }
 }
--- src/main/java/ru/job4j/serialization/json/TypeEngine.java
+++ src/main/java/ru/job4j/serialization/json/TypeEngine.java
@@ -7,6 +7,10 @@ public class TypeEngine {
         this.type = type;
     }
 
+    public String getType() {
+        return type;
+    }
+
     @Override
     public String toString() {
         return "TypeEngine{"
