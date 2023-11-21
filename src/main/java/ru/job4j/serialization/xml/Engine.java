1624f38e44ad98a699236f00e2b5e3cef1497269
dmn800 <nefediev.avid@yandex.ru>
2023-11-21 20:46:00 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/Engine.java
+++ src/main/java/ru/job4j/serialization/xml/Engine.java
@@ -1,15 +1,33 @@
 package ru.job4j.serialization.xml;
 
-import ru.job4j.serialization.json.TypeEngine;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "engine")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Engine {
-    private final boolean electric;
-    private final double price;
-    private final String company;
-    private final TypeEngine type;
-    private final String[] features;
+    @XmlAttribute
+    private boolean electric;
+
+    @XmlAttribute
+    private double price;
+
+    @XmlAttribute
+    private String company;
+
+    private TypeEngine type;
+
+    @XmlElementWrapper(name = "features")
+    @XmlElement(name = "feature")
+    private String[] features;
+
+    public Engine() {
+    }
 
     public Engine(boolean electric, double price, String company, TypeEngine type, String[] features) {
         this.electric = electric;
@@ -30,11 +48,24 @@ public class Engine {
                 + '}';
     }
 
-    public static void main(String[] args) {
-        final Engine engine = new Engine(true,
-                1000000, "Tesla",
-                new TypeEngine("Electric"),
-                new String[] {"25 MW", "400 km"}
+    public static void main(String[] args) throws Exception {
+        final Engine engine = new Engine(true, 1000000, "Tesla",
+                new TypeEngine("Electric"), new String[] {"25 MW", "400 km"}
         );
+
+        JAXBContext context = JAXBContext.newInstance(Engine.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml;
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(engine, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Engine result = (Engine) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
--- src/main/java/ru/job4j/serialization/xml/TypeEngine.java
+++ src/main/java/ru/job4j/serialization/xml/TypeEngine.java
@@ -1,7 +1,15 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "type")
 public class TypeEngine {
-    private final String type;
+    @XmlAttribute
+    private String type;
+
+    public TypeEngine() {
+    }
 
     public TypeEngine(String type) {
         this.type = type;
