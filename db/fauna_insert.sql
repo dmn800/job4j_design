9bd8268a8b4a045d1c52e34e0da93bc0dee8f42a
dmn800 <nefediev.avid@yandex.ru>
2023-12-12 01:01:15 +0500

    0. Select [#6861]

---
+++ db/fauna_insert.sql
@@ -0,0 +1,10 @@
+insert into fauna(name, avg_age, discovery_date)
+values ('name1', 7000, '1500-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('name2fish', 12000, '2000-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('name3', 25000, '1000-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('fishname4', 15000, '1800-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('name5', 8000, null);
