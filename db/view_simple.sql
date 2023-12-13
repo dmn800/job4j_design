e529e4e7bd8e4f4010c31809f4cd8a321bcba17f
dmn800 <nefediev.avid@yandex.ru>
2023-12-14 00:07:11 +0500

    1. Представления [#504792]

---
+++ db/view_simple.sql
@@ -0,0 +1,51 @@
+create table car_bodies(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table car_engines(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table car_transmissions(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table cars(
+    id serial primary key,
+    name varchar(255),
+	body_id int references car_bodies(id),
+	engine_id int references car_engines(id),
+	transmission_id int references car_transmissions(id)
+);
+
+insert into car_bodies(name) values ('Sedan'), ('Crossover'), ('Jeep');
+insert into car_engines(name) values ('1.6L'), ('1.8L'), ('2.0L'), ('4.0L');
+insert into car_transmissions(name) values ('Mechanic'), ('Automatic'), ('Variator');
+
+insert into cars(name, body_id, engine_id, transmission_id) values
+('Lada', 1, 1, 1),
+('Mersedes', 1, null, 2),
+('BWM', 3, 4, 2),
+('Skoda', 1, 1, 1),
+('Lada', null, null, null),
+('BMW', 1, 2, 2),
+('KIA', 1, 2, 2),
+('Lexus', 3, 4, null);
+
+create view show_cars_sedan_asc as
+	select c.id,
+	c.name car_name,
+	b.name body_name,
+	e.name engine_name,
+	t.name transsmission_name
+	from cars c
+	left join car_bodies b on c.body_id = b.id
+	left join car_engines e on c.engine_id = e.id
+	left join car_transmissions t on c.transmission_id = t.id
+	where b.name = 'Sedan'
+	order by e.name;
+
+select * from show_cars_sedan_asc;
