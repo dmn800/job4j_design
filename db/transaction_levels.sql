9ec3d40471f6d1c1366b20590ed8ee3e0971f72f
dmn800 <nefediev.avid@yandex.ru>
2023-12-16 19:36:12 +0500

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ db/transaction_levels.sql
@@ -0,0 +1,27 @@
+psql --username=postgres
+\c trigger
+
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+insert into products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+insert into products (name, producer, count, price) VALUES ('product_2', 'producer_2', 15, 32);
+insert into products (name, producer, count, price) VALUES ('product_3', 'producer_3', 8, 115);
+
+set session transaction isolation level read uncommitted;
+begin transaction;
+begin transaction isolation level read committed;
+begin transaction isolation level repeatable read;
+begin transaction isolation level serializable;
+
+insert into products (name, count, price) VALUES ('product_4', 11, 64);
+delete from products where price = 115;
+update products set price = 75 where name = 'product_1';
+
+rollback;
+commit;
